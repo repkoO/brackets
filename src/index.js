154528394b10cfc04a76095096a99179f8659056
Kirill Medvedev <repkomail@gmail.com>
2024-08-17 00:31:32 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  let open = bracketsConfig.map(pair => pair[0]);
+  let close = bracketsConfig.map(pair => pair[1]);
+
+  for (let symbol of str) {
+    if (open.includes(symbol)) {
+      if (close.includes(symbol) && array[array.length - 1] === symbol) {
+        array.pop();
+      } else
+        array.push(symbol);
+    } else {
+      let currentOpenBr = open[close.indexOf(symbol)];
+      if (array.length === 0 || array.pop() !== currentOpenBr) {
+        return false;
+      }
+    }
+  }
+  return array.length === 0;
 }
